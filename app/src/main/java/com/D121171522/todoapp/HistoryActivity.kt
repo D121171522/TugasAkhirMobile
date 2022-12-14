b5eb42f52ff0277a7e40765ed8fb0e86d158a528
Billy <chenb17d@student.unhas.ac.id>
2022-12-14 23:26:14 +0800

    Done

---
+++ app/src/main/java/com/D121171522/todoapp/HistoryActivity.kt
@@ -0,0 +1,13 @@
+package com.D121171522.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todoapp.R
+
+class HistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
